db0996280280a50ea296aee57bb6711aa1595aa1
cameron <cameronshaw95@gmail.com>
2020-08-28 11:24:55 +1200

    update button to use react module

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 
-import Kanye from './Kanye'
+import Kanye from "./Kanye"
 import Cammy from "./Cammy"
 
 const App = () => {
   return (
     <div className="container">
       {/* <h1>React development has begun!</h1> */}
-      <button className="btn">Press Me!</button>
-      </div>
+      {/* <button className="btn">Press Me!</button> */}
+      <Cammy />
+    </div>
   )
 }
 
