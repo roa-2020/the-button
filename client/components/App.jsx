7116a54f6524e4645cdcf5491884c7852406af7f
Lewis Rosenberg Smith <lrosenbergsmith@gmail.com>
2020-08-28 11:29:40 +1200

    css changes, routes and images

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,22 @@
 import React from "react"
+// lewis
+import {HashRouter as Router, Route} from 'react-router-dom'
 
+import Form from './Form'
+import Het from './Het'
+//lewis
 import Kanye from './Kanye'
 import Cammy from "./Cammy"
 
 const App = () => {
   return (
+    <Router>
     <div className="container">
       {/* <h1>React development has begun!</h1> */}
       <button className="btn">Press Me!</button>
       </div>
+      <Route path='/het/' component={Het}/>
+    </Router>
   )
 }
 
