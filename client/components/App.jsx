218a076064b8a9c6270fe5008d8d31cfcf6fea74
steven <steven.norhed@gmail.com>
2020-08-28 10:33:27 +1200

    add kanye

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-
+import Kanye from './Kanye'
 const App = () => {
   return (
     <div className="container">
       {/* <h1>React development has begun!</h1> */}
       <button className="btn">Press Me!</button>
-    </div>
+      </div>
   )
 }
 
